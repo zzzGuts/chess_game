c36b9a4362820aa4343c1fa8e0af8b8fff47394e
zzzGuts <jakub.piwowarczyk@gmail.com>
2023-04-08 21:32:43 +0200

    Created Controller, changes in Window

--- Client/src/main/java/app/chess_game/View/Window.java
+++ Client/src/main/java/app/chess_game/View/Window.java
@@ -1,12 +1,24 @@
 package app.chess_game.View;
 
 import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Window extends Application {
+    private static Stage window;
+    private static Scene loginScene;
+
     @Override
     public void start(Stage stage) throws Exception {
-        stage.show();
+        window = stage;
+        window.setTitle("Chess Game!");
+        window.setOnCloseRequest(windowEvent -> {
+            Platform.exit();
+            System.exit(0);
+        });
+
+        window.show();
     }
 
     public static void main(String[] args) {
