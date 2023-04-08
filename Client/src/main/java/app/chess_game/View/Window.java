e0712030f9ce0f43129521f34ca718b4d7a1c43b
zzzGuts <jakub.piwowarczyk@gmail.com>
2023-04-08 21:12:23 +0200

    Created View, Controller and Model packages in Client

--- Client/src/main/java/app/chess_game/View/Window.java
+++ Client/src/main/java/app/chess_game/View/Window.java
@@ -1,4 +1,4 @@
-package app.chess_game;
+package app.chess_game.View;
 
 import javafx.application.Application;
 import javafx.stage.Stage;
@@ -7,5 +7,9 @@ public class Window extends Application {
     @Override
     public void start(Stage stage) throws Exception {
         stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
     }
 }
