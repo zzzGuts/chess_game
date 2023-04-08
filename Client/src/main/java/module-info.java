0bed145292839e4f06e706bed3e18b614826a605
zzzGuts <jakub.piwowarczyk@gmail.com>
2023-04-08 21:12:23 +0200

    Created View, Controller and Model packages in Client

--- Client/src/main/java/module-info.java
+++ Client/src/main/java/module-info.java
@@ -2,7 +2,6 @@ module app.chess_game {
     requires javafx.controls;
     requires javafx.fxml;
 
-
-    opens app.chess_game to javafx.fxml;
-    exports app.chess_game;
+    opens app.chess_game.View to javafx.fxml;
+    exports app.chess_game.View;
 }
